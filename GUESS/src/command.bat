cab72b201c2713d3f2e0453519d5e8673626f344
Duc Manh <manhtech264@gmail.com>
2024-08-01 13:44:32 +0700

    update

--- GUESS/src/command.bat
+++ GUESS/src/command.bat
@@ -1,5 +1,6 @@
 @echo off
 cd "GUESS/src/"
+set PATH=%PATH%;"C:\msys64\ucrt64\bin";"C:\msys64\ucrt64"
 g++ targuess1_guess.cpp person.cpp timer.cpp personTran.cpp -o targuess1_guess
 
 targuess1_guess.exe
