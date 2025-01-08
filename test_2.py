8ca2bd15c82950c2789b1bc9828b72a8de8c94fb
Duc Manh <manhtech264@gmail.com>
2025-01-08 17:26:21 +0700

    update

--- test_2.py
+++ test_2.py
@@ -23,7 +23,7 @@ test_ls = ['xak', 'xal', 'xam', 'xan']
 with open('test.txt', 'w', encoding = 'utf-8') as f:
     url = "http://192.168.1.2:4003/preprocess-train-dataset/"
     for filename in os.listdir('processed_csv'):
-        if 'xai' in filename or 'xan' in filename:
+        # if 'xai' in filename or 'xan' in filename:
             print ('-------------------------------')
             print (filename)
             # for item in test_ls:
